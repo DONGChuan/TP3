ce4c2b68e53a21c4a2127c29bd3d73029e02d686
DONGChuan <dongchuan55@gmail.com>
2013-11-18 10:00:24 -0500

    correct consulter_cara in chaine.c

--- TP3/chaine.c
+++ TP3/chaine.c
@@ -5,12 +5,6 @@
 	VIDER_CHAINE
 
 	Cette fonction vide la chaine recue.
-
-	PARAMETRE :
-		- ptr_chaine : L'adresse de la chaine a vider (t_chaine *).
-
-	RETOUR : Aucun. Cependant, la chaine a l'adresse recue ne contient plus
-			 de caracteres.
 */
 void vider_chaine(t_chaine * ptr_chaine)
 {
@@ -24,18 +18,10 @@ void vider_chaine(t_chaine * ptr_chaine)
 	ptr_chaine->nb_caracteres = 0;
 }
 
-
 /*
 	AJOUTER_CARACTERE
 
 	Cette fonction ajoute un caractere au bout de la chaine recue.
-
-	PARAMETRES :
-		- ptr_chaine : L'adresse de la chaine a modifier (t_chaine *).
-		- ajout      : Le caractere a ajouter a la chaine.
-
-	RETOUR : 1 en cas de succes et 0 si la chaine est pleine.
-
 */
 int ajouter_caractere(t_chaine * ptr_chaine, unsigned char ajout)
 {
@@ -52,38 +38,18 @@ int ajouter_caractere(t_chaine * ptr_chaine, unsigned char ajout)
 	}
 }
 
-
-
 /*
 	CONSULTER_CARACTERE
 
 	Cette fonction consulte le caractere dans une chaine a l'indice demande.
-
-	PARAMETRES :
-		- ptr_chaine : L'adresse de la chaine a consulter (const t_chaine *).
-		- indice     : L'indice du caractere a consulter (int).
-					   Si cet indice n'est pas dans l'intervalle
-					   [0, taille_chaine(ptr_chaine)[, le programme se termine.
-	    - ptr_caractere_lu : (SORTIE) L'adresse ou le caractere lu doit etre
-							 ecrit en cas de succes (unsigned char *).
-
-	RETOUR : Le caractere a l'indice demande en cas de succes. Si l'indice n'est
-			 pas valide, le programme se termine (assert).
-
-	ATTENTION : La fonction suppose que le pointeur pointe vers une chaine
-			    initialisee a l'aide de la fonction vider_chaine.
 */
 unsigned char consulter_caractere(const t_chaine * ptr_chaine, int indice)
 {
 	assert(indice >= 0);
 	assert(indice < TAILLE_MAXIMALE_CHAINE);
 
-	unsigned char ptr_caractere_lu;
-	ptr_caractere_lu = ptr_chaine->caracteres[indice];
-
-	return ptr_caractere_lu;
+	return ptr_chaine->caracteres[indice];
 }
-//asaaaa
 
 /*
 	TAILLE_CHAINE
